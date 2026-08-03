ea9389b62c043c61c722cf592a4ea2e2a9c58550
Devraj Singh Sodha <devrajsinghsodha30@gmail.com>
2026-08-03 23:48:29 +0530

    Today's wotk-making main,cpp

--- main.cpp
+++ main.cpp
@@ -1,2 +1,19 @@
-// #This is ypour working space
+#include<bits/stdc++.h>
 #include "mess_complain.cpp"
+using namespace std;
+
+class Complain_Manegement{
+    string complain_id;
+    string student_name;
+    bool status = false;
+    string complain_title;
+    
+};
+
+int main(){
+
+    Mess_complain m1;
+    m1.run();
+
+    return 0;
+}
